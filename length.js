6ccd41acce06f5f18a17a700ccc807a161b383e5
2-2-gif <kjmcapitals1@gmail.com>
2026-01-15 14:31:59 +0200

    Add files via upload

---
+++ length.js
@@ -0,0 +1,25 @@
+function(e) {
+    function t(e) {
+        if(e.length > 0) {
+            var t = document.querySelector(e);
+                }
+            }
+        }
+function n(e) {
+    var t = null;
+    document.head?c(document.head,e):c(e)
+    document.getElementById("heightl")
+        }
+function c(e,t) {
+    if (e&&t) {
+        var n = document.createElement("style");
+        n.setAttribute("type", "text/css")
+        e.appendChild(n);
+        for(var c = t.split("^^^"),o=0; o<c.length; ++o) {
+            var i = c[o],r=document.getElementsByTagName(i);
+            for(var a=0; a<r.length; a++) {
+                var u = r[a];
+                    }
+                }
+            }
+        }
